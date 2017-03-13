206af4df0b9a2133c458d6e6ec68e6abffd12029
Goce Mitevski <goce.mitevski@gmail.com>
2017-03-13 12:08:46 +0100

    Add remove-extra-fields.php

---
+++ remove-extra-feeds.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+  Plugin Name: Remove Extra Feeds
+  Plugin URI:  https://github.com/keitaroinc/remove-extra-feeds
+  Description: Remove all extra syntication feeds and serve only the main WordPress feed
+  Version:     20170313
+  Author:      Goce Mitevski
+  Author URI:  https://github.com/gocemitevski
+  License:     GPL2
+  License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+  exit;
+}
+
+function remove_extra_feeds() {
+
+  // Automatically add feed links in <head> if not already added by the theme
+  if (!current_theme_supports('automatic-feed-links')) {
+    add_theme_support('automatic-feed-links');
+  }
+
+  remove_action('wp_head', 'feed_links_extra', 3);
+  add_action('feed_links_show_comments_feed', '__return_false');
+}
+
+add_action('after_setup_theme', 'remove_extra_feeds');
