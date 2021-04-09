bd687eae6b82a7748762d5f4f390856d74be2748
Elena Kusevska <31021042+ElenaKusevska@users.noreply.github.com>
2021-04-09 12:33:50 +0200

    Update remove-extra-feeds.php

--- remove-extra-feeds.php
+++ remove-extra-feeds.php
@@ -15,7 +15,6 @@
  * Version:     20170313
  * Author:      Goce Mitevski
  * Author URI:  https://github.com/gocemitevski
- * Text Domain: plugin-slug
  * License:     GPL v2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
